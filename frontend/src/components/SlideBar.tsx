d926a8df2872d7d65dbab0881a5ff24a0f30c896
sarthak <sarthakssingh49@gmail.com>
2026-08-30 09:06:10 +0530

    minor polish

--- frontend/src/components/SlideBar.tsx
+++ frontend/src/components/SlideBar.tsx
@@ -8,9 +8,9 @@ const SlideBar = () => {
   return (
     <div className='w-75 min-h-screen border border-gray-200'>
         <div className=" pr-3 border-amber-900 flex flex-col gap-2 mt-8 pl-4">
-            <Button variant="ghost" className="w-full justify-start rounded-sm"> <Notebook /> <Link to ={`/projects/2`}>Flags</Link> </Button>
-            <Button variant="ghost" className="w-full justify-start rounded-sm"> <Users /> <Link to={`/projects/2/segments`}> Segments </Link> </Button>
-            <Button variant="secondary" className="w-full justify-start rounded-sm"> <NotebookPen /> <Link to = {`/projects/2/guardrails`}>Guardrails</Link> </Button>
+          <Link to ={`/projects/2`}>  <Button variant="ghost" className="w-full justify-start rounded-sm"> <Notebook /> Flags </Button> </Link>
+          <Link to={`/projects/2/segments`}>  <Button variant="ghost" className="w-full justify-start rounded-sm"> <Users />  Segments  </Button> </Link>
+          <Link to={`/projects/2/guardrails`}>  <Button variant="secondary" className="w-full justify-start rounded-sm"> <NotebookPen /> Guardrails </Button> </Link>
             <Button variant="ghost" className="w-full justify-start rounded-sm"> <UserCog /> Members</Button>
             <Button variant="ghost" className="w-full justify-start rounded-sm"> <Lightbulb /> Audit Trails</Button>
             
--- frontend/src/pages/GuardrailDetails.tsx
+++ frontend/src/pages/GuardrailDetails.tsx
@@ -2,7 +2,16 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Separator } from '@/components/ui/separator'
 import { Slider } from '@/components/ui/slider'
 import { Switch } from '@/components/ui/switch'
+import { Pause, SquareArrowRightExit } from 'lucide-react'
 import React, { useState } from 'react'
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Checkbox } from '@/components/ui/checkbox'
 
 const GuardrailDetails = () => {
 
@@ -10,46 +19,27 @@ const GuardrailDetails = () => {
     const [reducingRollout, setReducingRollout] = useState(true);
     return (
         <div className='min-h-screen px-10 py-5 bg-gray-50'>
-            <div className=''>
+            <div className='flex items-center justify-center'>
                 <div className='text-2xl font-medium'> New checkout guardrail </div>
 
+                <div className='ml-auto flex gap-3'>
+                <button className='  text-blue-700 border border-blue-600 flex items-center gap-1 px-3 py-1 font-medium text-sm transition-colors duration-200 rounded-sm cursor-pointer'>  <div>Pause</div> </button>
+                </div>
             </div>
 
             <Separator className="my-2 mt-5" />
 
-            <div className='mt-10 h-100 bg-white py-5 px-5 flex border-red-400'>
+            <div className='mt-10 h-100  py-5 px-5 flex border-red-400'>
                 <div className='w-180  pr-3 border-amber-500 '>
                     <div>
                         <div className='text-xl font-semibold'> Triggers </div>
                         <div className='text-muted-foreground text-sm mt-1'> The set of conditions which a user has to follow to lie in this segment. </div>
                     </div>
 
-                    <div className='border border-gray-200 text-sm rounded-sm flex h-70 py-3 px-3 mt-5 gap-40'>
-                        <div className='text-muted-foreground '>
+                    <div className='border border-gray-200 text-sm rounded-sm flex flex-col gap-5 h-70 py-3 px-3 mt-5 '>
+                        <div className='flex gap-40 pl-17'>
+                            <div className='text-muted-foreground '>
                             <div> Metric </div>
-
-                            <div className='text-foreground border border-gray-300 mt-3 rounded-xs'>
-                                <Select defaultValue="lat">
-
-                                    <SelectTrigger className="w-25 justify-between">
-                                        <SelectValue />
-                                    </SelectTrigger>
-
-                                    <SelectContent position='popper'>
-                                        <SelectItem value="err">
-                                            Error Rate
-                                        </SelectItem>
-                                        <SelectItem value="tra">
-                                            Traffic
-                                        </SelectItem>
-                                        <SelectItem value="lat">
-                                            Latency
-                                        </SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-
-
 
                         </div>
                         <div className='text-muted-foreground'>
@@ -57,6 +47,34 @@ const GuardrailDetails = () => {
                         </div>
                         <div className='text-muted-foreground'>
                             Time Window
+                        </div>
+                        </div>
+
+                        <div className='border-red-400 text-foreground flex flex-col gap-3'>
+                            <div className='flex border bg-gray-100 rounded-xs h-15 items-center pl-2  gap-9'>
+                                <Checkbox className='size-6 cursor-pointer' />
+                                <div className='flex '>
+                                    <div className=''> Error Rate reaches </div>
+                                    <div className='ml-25'> 5% </div>
+                                    <div className='ml-45'> within 30 minutes </div>
+                                </div>
+                            </div>
+                            <div className='flex border bg-gray-100 rounded-xs h-15 items-center pl-2 gap-9'>
+                                <Checkbox className='size-6 cursor-pointer' />
+                                <div className='flex '>
+                                    <div className=''> Latency reaches </div>
+                                    <div className='ml-25'> 500ms </div>
+                                    <div className='ml-43'> within 30 minutes </div>
+                                </div>
+                            </div>
+                            <div className='flex border bg-gray-100 rounded-xs h-15 items-center pl-2 gap-9'>
+                                <Checkbox className='size-6 cursor-pointer' />
+                                <div className='flex '>
+                                    <div className=''> Traffic spikes upto </div>
+                                    <div className='ml-18'> 10K requests </div>
+                                    <div className='ml-37'> within 10 minutes </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -67,13 +85,14 @@ const GuardrailDetails = () => {
                     <div className='text-xl font-semibold'>  Actions </div>
                     <div className='text-muted-foreground text-sm mt-1'> Actions which will be executed. </div>
                     <div className='border text-sm border-gray-200 rounded-sm flex flex-col h-70 py-3 px-3 mt-5'>
-                        <div className='mb-2'>
-                            <div className='flex items-center h-10 gap-3'> <Switch /> <div> Turn Off Feature Flag </div> </div>
+                        <div className='mb-4 mt-1'>
+                            <div className='flex items-center h-6 gap-3'> <Switch className="cursor-pointer"/> <div> Kill Switch </div> </div>
+                            <div className='ml-11 text-xs text-muted-foreground '> Instantly turn off feature flag </div>
                         </div>
                         <Separator />
-                        <div className='mt-2'>
-                            <div className='flex items-center h-10 gap-3 '> <Switch checked={reducingRollout} onCheckedChange={() => setReducingRollout((prev) => !prev)} /> <div> Reduce Rollout Percentage </div> </div>
-                            <div className='mt-1'>
+                        <div className='mt-2 mb-6'>
+                            <div className='flex items-center h-10 gap-3 '> <Switch className="cursor-pointer" checked={reducingRollout} onCheckedChange={() => setReducingRollout((prev) => !prev)} /> <div> Rollback </div> </div>
+                            <div className=''>
                                 <Slider
                                     defaultValue={[75]}
                                     max={100}
@@ -81,6 +100,12 @@ const GuardrailDetails = () => {
                                     className="mx-auto w-full max-w-xs" disabled={!reducingRollout}
                                 />
                             </div>
+                            <div className='ml-11 text-xs text-muted-foreground mt-2'> Reduce rollout percentage to selected value </div>
+                        </div>
+                         <div className="my-2 h-[0.5px] w-full bg-border" />
+                        <div className='mt-3 flex flex-col gap-0'>
+                            <div className='flex  items-center h-6 gap-3'> <Switch className="cursor-pointer"/> <div> Send Notification </div> </div>
+                            <div className='ml-11 text-xs text-muted-foreground '> Update audit trails to notify your team members </div>
                         </div>
                     </div>
                 </div>
